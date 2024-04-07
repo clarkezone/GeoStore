66fb94d4b40d50afd256e17af5742968053ef843
James Clarke <james@clarkezone.net>
2024-04-06 17:43:10 -0700

    Add a test for parsing

--- src/GeoStore.Tests/UnitTest1.cs
+++ src/GeoStore.Tests/UnitTest1.cs
@@ -1,10 +1,31 @@
+using System.Text.Json;
+using GeoStore.Core;
+
 namespace GeoStore.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestDeserializationOfJsonFragments()
     {
+        var fileName = "TestData//payloads.json"; // The name of your file
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
 
+        // Now you can use filePath as the path to your file
+        Assert.True(File.Exists(filePath), "Expected data file not found.");
+
+        var jsonContent = File.ReadAllText(filePath);
+        var jsonFragments = jsonContent.Split(new string[] { "***" }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Act
+        var rootObjects = jsonFragments.Select(fragment => JsonSerializer.Deserialize<RootObject>(fragment)).ToList();
+
+        // Assert
+        Assert.NotNull(rootObjects);
+        Assert.All(rootObjects, item => Assert.NotNull(item));
+        //TODO: fix this make these test better
+        //Assert.True(rootObjects.All(ro => ro.Locations != null)); // Example assertion, adjust as needed
+
+        // Additional assertions as per your requirements
     }
 }
