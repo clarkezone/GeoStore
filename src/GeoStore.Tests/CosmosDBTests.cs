c80b33261128cfcfce2770435fe8a60de3cbfeeb
James Clarke <james@clarkezone.net>
2024-04-15 11:49:11 -0700

    Avoid cert

--- src/GeoStore.Tests/CosmosDBTests.cs
+++ src/GeoStore.Tests/CosmosDBTests.cs
@@ -12,9 +12,20 @@ public class DatabaseFixture : IDisposable
     public DatabaseFixture()
     {
         //TODO: start docker container
-        client = new(
-                   accountEndpoint: "https://localhost:8081/",
-                   authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+
+        CosmosClientOptions options = new()
+        {
+            HttpClientFactory = () => new HttpClient(new HttpClientHandler()
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            }),
+            ConnectionMode = ConnectionMode.Gateway,
+        };
+
+        client = new CosmosClient(
+                accountEndpoint: "https://localhost:8081/",
+                authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                clientOptions: options
                );
     }
 
